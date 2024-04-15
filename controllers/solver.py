9f6696556be25c1d3d46f8c1a6420fe9bcaf1bf0
hishi <95124996+hish1@users.noreply.github.com>
2024-04-15 17:30:31 +1000

    Add files via upload

--- controllers/solver.py
+++ controllers/solver.py
@@ -1,62 +1,118 @@
+from math import isnan
 from app import app
 from flask import render_template, request, session
 
 from utils import get_db_connection
-from models.bz_model import *
+from models.solver_model import *
 
-global value_checked
-global checked
-checked, value_checked = [], []
+chosen_data = {}
 
 @app.route('/solver/', methods=['get', 'post'])
 def solver():
-    global checked
-    global value_checked
+    global chosen_data
     conn = get_db_connection()
     data_features = get_features(conn)
-    data_checked = checked
-    data_features_sc = get_features_sc(conn)
-    data_features_num = get_features_num(conn)
+    data_features_sc = []
+    data_features_num = []
     data_possible_sc = []
     data_possible_num = []
-    data_value_checked = value_checked
-    data_all = []
-    result = ''
-
-
-    if request.values.getlist('get_checkbox'):
-        checked = request.values.getlist('get_checkbox')
-        data_checked = checked
+    flag = False
+    result_good = []
+    result_bad = []
 
 
     if request.values.get('feature_selector'):
-        feature_id = int(request.values.get('features_selector'))
-        data_possible_sc = get_possible_value_sc(feature_id, conn)
+        feature_id = int(request.values.get('feature_selector'))
         session['feature_id'] = feature_id
-        
+        session['checked_id'] = -1
 
-    if request.values.get('value_checker'):
+        sc = get_features_sc_id(feature_id, conn)
+        num = get_features_num_id(feature_id, conn)
+        if not sc.empty:
+            data_possible_sc = get_possible_value_sc(feature_id, conn)
+        elif not num.empty:
+            data_possible_num = get_possible_value_num(feature_id, conn)
+
+
+    elif request.values.get('value_choose'):
         feature_id = int(session.get('feature_id'))
-        value_checked = request.values.get('value_checker')
-        data_value_checked = value_checked
-        
 
-    if request.values.get('get_result'):
-        a=1
+        sc = get_features_sc_id(feature_id, conn)
+        num = get_features_num_id(feature_id, conn)
+        if not sc.empty:
+            data_possible_sc = get_possible_value_sc(feature_id, conn)
+            option = int(request.values.get('value_choose'))
+            chosen_data[feature_id] = data_possible_sc.values[option-1][1]
+            session['checked_id'] = option
+        elif not num.empty:
+            data_possible_num = get_possible_value_num(feature_id, conn)
+            option = request.fvalues.ge('value_choose')
+            chosen_data[feature_id] = int(option)
+            session['checked_id'] = int(request.values.get('label'))
+
+
+    elif request.values.get('get_result'):
+        flag = True
+
+        types = get_types(conn)
+        types = types.values
+        res_bad = []
+        res_good = []
+
+        for type in types:
+            f = True
+            res = []
+            for v in chosen_data:
+                f = False
+                t = type[0]
+                n = chosen_data[v]
+                val = get_features_id(v, conn).values[0]
+                if get_type_values(t, v, n, conn).empty:
+                    res.append(f''' - значение признака «{val[1]}» не подходит\n''')
+
+            if res != []:
+                res_bad.append(f'''\nКласс «{type[1]}» не подходит так как:\n''')
+                res_bad += res
+            else:
+                res_good.append(f'«{type[1]}»\n')
+            
+        if res_good == []:
+            result_good.append('Нет классов\n')
+        elif chosen_data == {}:
+            result_good.append('Не выбрано ни одного признака.\n')
+        else:
+            result_good = res_good
+            result_bad = res_bad
+
+
+    elif not data_features.empty:
+        session['feature_id'] = int(data_features.loc[0, 'feature_id'])
+        feature_id = session['feature_id']
+        session['checked_id'] = -1
+
+        sc = get_features_sc_id(feature_id, conn)
+        num = get_features_num_id(feature_id, conn)
+        if not sc.empty:
+            data_possible_sc = get_possible_value_sc(feature_id, conn)
+        elif not num.empty:
+            data_possible_num = get_possible_value_num(feature_id, conn)
+
+        
 
 
 
     html = render_template(
         'solver.html',
         data_features = data_features,
-        data_checked = data_checked,
         data_features_sc = data_features_sc,
         data_features_num = data_features_num,
         data_possible_sc = data_possible_sc,
         data_possible_num = data_possible_num,
-        data_all = data_all,
-        result = result,
+        result_good = result_good,
+        result_bad = result_bad,
+        flag = flag,
         feature_id = session['feature_id'],
+        checked_id = session['checked_id'],
         len = len
     )
     return html
--- models/solver_model.py
+++ models/solver_model.py
@@ -10,7 +10,23 @@ def get_features_sc(conn):
 def get_features_num(conn):
     return pandas.read_sql(f'''select * from feature where type=1''', conn)
 
+def get_features_id(id, conn):
+    return pandas.read_sql(f'''select * from feature where feature_id={id}''', conn)
+def get_features_sc_id(id, conn):
+    return pandas.read_sql(f'''select * from feature where type=0 and feature_id={id}''', conn)
+def get_features_num_id(id, conn):
+    return pandas.read_sql(f'''select * from feature where type=1 and feature_id={id}''', conn)
+
 def get_possible_value_sc(id, conn):
     return pandas.read_sql(f'''select * from possible_value where feature_id={id}''', conn)
 def get_possible_value_num(id, conn):
     return pandas.read_sql(f'''select * from num_possible_value where feature_id={id}''', conn)
+
+def get_type_values(type_id, feature_id, value_n, conn):
+    return pandas.read_sql(f'''select feature_id, value_id
+                           from type
+                           natural join feature
+                           natural join features_list
+                           natural join value
+                           where type_id={type_id} and feature_id={feature_id} and value='{value_n}'
+                           ''', conn)
